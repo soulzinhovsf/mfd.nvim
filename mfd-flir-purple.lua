8c06c8b8ccd1f7886d2611f53d21600c3cd95f14
Jean <163385123+soulzinhovsf@users.noreply.github.com>
2026-02-12 04:07:09 -0300

    Add files via upload

---
+++ mfd-flir-purple.lua
@@ -0,0 +1,34 @@
+-- lualine theme for mfd-flir-rh colorscheme
+-- dark grey (#181818) bg, soft mid grey (#909090) fg, purple spots
+return {
+  normal = {
+    a = { fg = '#181818', bg = '#909090', gui = 'bold' },
+    b = { fg = '#909090', bg = '#242424' },
+    c = { fg = '#909090', bg = '#1C1C1C' },
+  },
+  insert = {
+    a = { fg = '#181818', bg = '#9f43ba', gui = 'bold' },
+    b = { fg = '#909090', bg = '#242424' },
+    c = { fg = '#909090', bg = '#1C1C1C' },
+  },
+  visual = {
+    a = { fg = '#181818', bg = '#909090', gui = 'bold' },
+    b = { fg = '#909090', bg = '#242424' },
+    c = { fg = '#909090', bg = '#1C1C1C' },
+  },
+  replace = {
+    a = { fg = '#181818', bg = '#43134d', gui = 'bold' },
+    b = { fg = '#909090', bg = '#242424' },
+    c = { fg = '#909090', bg = '#1C1C1C' },
+  },
+  command = {
+    a = { fg = '#181818', bg = '#909090', gui = 'bold' },
+    b = { fg = '#909090', bg = '#242424' },
+    c = { fg = '#909090', bg = '#1C1C1C' },
+  },
+  inactive = {
+    a = { fg = '#505050', bg = '#1C1C1C' },
+    b = { fg = '#505050', bg = '#1C1C1C' },
+    c = { fg = '#505050', bg = '#1C1C1C' },
+  },
+}
